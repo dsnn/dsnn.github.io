6296cc4ea149aace9894af8259d654367004b5dc
dsn <dsn@dsnn.io>
2020-09-29 22:24:56 +0200

    Add simple logo

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,34 @@
-import React from "react"
+import React from "react";
+import styled, { createGlobalStyle } from "styled-components";
+
+const GlobalStyle = createGlobalStyle`
+  html, body {
+    height: 100%;
+    margin: 0;
+    padding: 0;
+  }
+`;
+
+const Container = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  padding: 0;
+  margin: 0;
+  height: 100vh;
+`;
+
+const Logo = styled.div`
+  background-image: url("/logo.svg");
+  width: 100px;
+  height: 100px;
+`;
 
 export default function Home() {
-  return <div>Hi</div>
+  return (
+    <Container>
+      <GlobalStyle />
+      <Logo />
+    </Container>
+  );
 }
